657267c0863ba68a1f4c9a38f7506f47e6ec02de
vignan18 <saivignanreddy0@gmail.com>
2025-11-02 12:47:40 +0530

    Initial env test app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h2>React Env Variable Test</h2>
+      <p><b>Mode:</b> {import.meta.env.MODE}</p>
+      <p><b>VITE_APP_MODE:</b> {import.meta.env.VITE_APP_MODE}</p>
+      <p><b>VITE_APP_NAME:</b> {import.meta.env.VITE_APP_NAME}</p>
+      <p><b>VITE_SECRET_KEY:</b> {import.meta.env.VITE_SECRET_KEY}</p>
+    </div>
+  );
+}
+
+export default App;
