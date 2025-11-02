66e20c940bcf2693e95550c200d241016e1c4d96
vignan18 <saivignanreddy0@gmail.com>
2025-11-02 13:24:36 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
+  console.log("import.meta.env:");
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h2>React Env Variable Test</h2>
+      <h2>React Env Variable Test check</h2>
       <p><b>Mode:</b> {import.meta.env.MODE}</p>
       <p><b>VITE_APP_MODE:</b> {import.meta.env.VITE_APP_MODE}</p>
       <p><b>VITE_APP_NAME:</b> {import.meta.env.VITE_APP_NAME}</p>
